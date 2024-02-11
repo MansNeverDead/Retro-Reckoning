7982ef98342e4e04029c4099d67f5b9ca88ed067
MansNeverDead <nagadioyetti@gmail.com>
2024-02-11 11:32:00 -0300

    testing

---
+++ Game/Menus/Settings_Menu/Settings.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_controls_pressed():
+	pass # Replace with function body.
+
+
+func _on_volume_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Menus/Main_Menu/main_menu.tscn")
