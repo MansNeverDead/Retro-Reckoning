c2e1cc9f17ef9a8e30f2d4a6c5f624656f755070
Sleety <89317270+The-Sleety@users.noreply.github.com>
2024-03-10 15:41:00 +0300

    Added fixed main menu

--- Game/OverWorldCMDS/Menus/Main_Menu/Main_Menu.gd
+++ Game/OverWorldCMDS/Menus/Main_Menu/Main_Menu.gd
@@ -8,10 +8,10 @@ func _physics_process(delta):
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Menus/Story Mode/story_mode.tscn")
+	get_tree().change_scene_to_file("res://OverWorldCMDS/Menus/Story Mode/story_mode.tscn")
 
 func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://Menus/Settings_Menu/Settings.tscn")
+	get_tree().change_scene_to_file("res://OverWorldCMDS/Menus/Settings_Menu/Settings.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
